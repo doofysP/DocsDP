c0f7a30c82715e5d27a7923b05772d05cb7ad130
DisoQ <juro855@gmail.com>
2023-04-27 16:25:36 -0500

    Se actualiza botón de guías por categorías

--- docs/guias/instalacion/categorias.ts
+++ docs/guias/instalacion/categorias.ts
@@ -3,5 +3,10 @@ export const categoria0 = [
     title: '💿 Metodo Rufus',
     description: 'Este proceso es mayormente necesario para que nuestra USB sirva como un arranque al sistema que busquemos instalar.',
     link: '/guias/instalacion/memorias-y-particiones/metodo-rufus',
+  },
+  {
+    title: '💿 Metodo Ventoy',
+    description: 'Proceso similar al que se practica con Rufus, sin embargo, Ventoy permite alojar mayor numero de sistemas dentro del almacenamiento.',
+    link: '/guias/instalacion/memorias-y-particiones/metodo-ventoy',
   }
 ];
