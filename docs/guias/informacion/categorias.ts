3b2377f07d19cd0034c45066fc65972ba2dce956
DisoQ <juro855@gmail.com>
2023-05-15 15:36:50 -0500

    Añadir botón de Office

--- docs/guias/informacion/categorias.ts
+++ docs/guias/informacion/categorias.ts
@@ -1,12 +1,18 @@
 export const categoria0 = [
   {
-    title: '💾 Tengo UEFI o BIOS',
-    description: '¿Cómo sé que mi equipo cuenta con UEFI o Legacy (BIOS)?',
-    link: '/guias/informacion/bios-uefi',
+    title: "💾 Tengo UEFI o BIOS",
+    description: "¿Cómo sé que mi equipo cuenta con UEFI o Legacy (BIOS)?",
+    link: "/guias/informacion/bios-uefi",
   },
   {
-    title: '🖥️ Requisitos de Windows',
-    description: 'Requisitos recomendados para Windows en sus diferentes versiones',
-    link: '/guias/informacion/requisitos-windows',
+    title: "🖥️ Requisitos de Windows",
+    description:
+      "Requisitos recomendados para Windows en sus diferentes versiones",
+    link: "/guias/informacion/requisitos-windows",
+  },
+  {
+    title: "📚 Office",
+    description: "Conoce las diferentes versiones de Office y sus diferencias.",
+    link: "/guias/informacion/office",
   },
 ];
