184f2124f2c984fc01ad3bc3dfe5f4b8dbd26c0d
= <=>
2024-08-21 19:27:35 -0500

    Remover historial de cambios

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -1,15 +1,12 @@
 export function nav() {
     return [
+      { text: "Inicio", link: "/" },
       { text: "Guías", link: "/guias/", activeMatch: "/guias/" },
       {
         text: "Colaboración",
         activeMatch: "/colaboracion",
         link: "/colaboracion"
-      },
-      {
-        text: "Historial de cambios",
-        link: "https://github.com/doofysP/GuiasDP/blob/main/CHANGELOG.md",
-      },
+      }
     ];
   }
   
